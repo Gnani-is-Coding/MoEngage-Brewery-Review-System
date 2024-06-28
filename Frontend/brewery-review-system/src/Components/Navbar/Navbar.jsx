35db219a2517c515320fe3a801c0eae8a721395e
Gnani-is-Coding <gnanendragariminti@nexocide.com>
2024-06-28 08:48:31 +0530

    completes login page

--- Frontend/brewery-review-system/src/Components/Navbar/Navbar.jsx
+++ Frontend/brewery-review-system/src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import './Navbar.css'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'>
+      <nav className='navbar'>
+          <h1 className='navbar-heading'>Brewery Reviews</h1>
+      </nav>  
+    </div>
+    
   )
 }
 
