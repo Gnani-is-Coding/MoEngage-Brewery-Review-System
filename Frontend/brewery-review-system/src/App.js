ad2b103bc04404e46b0df5dac9a7128c00cd240b
Gnani-is-Coding <gnanendragariminti@nexocide.com>
2024-06-28 09:32:38 +0530

    adds UI of login n signup

--- Frontend/brewery-review-system/src/App.js
+++ Frontend/brewery-review-system/src/App.js
@@ -1,13 +1,16 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './Components/Login/Login';
-
+import Home from './Components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Login/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </Router>
   );
 }
 
--- Frontend/brewery-review-system/src/Components/Login/Login.jsx
+++ Frontend/brewery-review-system/src/Components/Login/Login.jsx
@@ -4,7 +4,8 @@ import Navbar from '../Navbar/Navbar'
 import './Login.css'
 
  function Login() {
-    const [showPassword, setShowPassword] = useState(false) 
+    const [showPassword, setShowPassword] = useState(false)
+    const [currentView, setCurrentView] = useState('SIGN-Up')
    
     return(
         <div className='login-container'>
@@ -16,7 +17,8 @@ import './Login.css'
             <h1 className="brewery-heading">Brewery.</h1>
         </div>
 
-        <div className="form-container">
+        {currentView === 'SIGN-IN' ?
+        ( <div className="form-container">
             <div className="form-card-container">
                 <h1 className="sign-in-heading">Sign in</h1>
                 <p className="sign-in-para">Sign in to your account</p>
@@ -69,7 +71,7 @@ import './Login.css'
             <span className="span">Forgot password?</span>
         </div>
         <button className="button-submit" type="submit">Sign In</button>
-        <p className="p">Don't have an account? <span className="span">Sign Up</span></p>
+        <p className="p">Don't have an account? <span className="span" onClick={() => setCurrentView('SIGN-UP')}>Sign Up</span></p>
         
         <p className="p line">Or With</p>
 
@@ -108,7 +110,113 @@ import './Login.css'
     </div>
     </form>
     </div>
-        </div>
+        </div>) : (
+             <div className="form-container">
+             <div className="form-card-container">
+                 <h1 className="sign-in-heading">Sign Up</h1>
+                 <p className="sign-in-para">Create a new account</p>
+ 
+         <form className="form">
+         <div className="flex-column">
+                 <label>User name </label>
+             </div>
+ 
+             <div className="inputForm">
+                 {/* <svg height="20" viewBox="0 0 32 32" width="20" xmlns="http://www.w3.org/2000/svg"><g id="Layer_3" data-name="Layer 3">
+                     <path d="m30.853 13.87a15 15 0 0 0 -29.729 4.082 15.1 15.1 0 0 0 12.876 12.918 15.6 15.6 0 0 0 2.016.13 14.85 14.85 0 0 0 7.715-2.145 1 1 0 1 0 -1.031-1.711 13.007 13.007 0 1 1 5.458-6.529 2.149 2.149 0 0 1 -4.158-.759v-10.856a1 1 0 0 0 -2 0v1.726a8 8 0 1 0 .2 10.325 4.135 4.135 0 0 0 7.83.274 15.2 15.2 0 0 0 .823-7.455zm-14.853 8.13a6 6 0 1 1 6-6 6.006 6.006 0 0 1 -6 6z">
+                 </path></g></svg> */}
+                 <input type="text" className="input" placeholder="Set your username"/>
+             </div>
+
+             <div className="flex-column">
+                 <label>Email </label>
+             </div>
+ 
+             <div className="inputForm">
+                 <svg height="20" viewBox="0 0 32 32" width="20" xmlns="http://www.w3.org/2000/svg"><g id="Layer_3" data-name="Layer 3">
+                     <path d="m30.853 13.87a15 15 0 0 0 -29.729 4.082 15.1 15.1 0 0 0 12.876 12.918 15.6 15.6 0 0 0 2.016.13 14.85 14.85 0 0 0 7.715-2.145 1 1 0 1 0 -1.031-1.711 13.007 13.007 0 1 1 5.458-6.529 2.149 2.149 0 0 1 -4.158-.759v-10.856a1 1 0 0 0 -2 0v1.726a8 8 0 1 0 .2 10.325 4.135 4.135 0 0 0 7.83.274 15.2 15.2 0 0 0 .823-7.455zm-14.853 8.13a6 6 0 1 1 6-6 6.006 6.006 0 0 1 -6 6z">
+                 </path></g></svg>
+                 <input type="text" className="input" placeholder="Enter your Email"/>
+             </div>
+         
+             <div className="flex-column">
+             <label>Password </label>
+             </div>
+ 
+         <div className="inputForm">
+             <svg height="20" viewBox="-64 0 512 512" width="20" xmlns="http://www.w3.org/2000/svg">
+             <path d="m336 512h-288c-26.453125 0-48-21.523438-48-48v-224c0-26.476562 21.546875-48 48-48h288c26.453125 0 48 21.523438 48 48v224c0 26.476562-21.546875 48-48 48zm-288-288c-8.8125 0-16 7.167969-16 16v224c0 8.832031 7.1875 16 16 16h288c8.8125 0 16-7.167969 16-16v-224c0-8.832031-7.1875-16-16-16zm0 0">
+             </path><path d="m304 224c-8.832031 0-16-7.167969-16-16v-80c0-52.929688-43.070312-96-96-96s-96 43.070312-96 96v80c0 8.832031-7.167969 16-16 16s-16-7.167969-16-16v-80c0-70.59375 57.40625-128 128-128s128 57.40625 128 128v80c0 8.832031-7.167969 16-16 16zm0 0">
+             </path>
+             </svg>  
+ 
+             <input type={showPassword ? "text":"password"} className="input" placeholder="Set your Password"/>
+ 
+             {showPassword ?  <svg width="20px" onClick={() => setShowPassword(false)} height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+             <g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round">
+                 </g><g id="SVGRepo_iconCarrier"> <path d="M2 2L22 22" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> 
+                 <path d="M6.71277 6.7226C3.66479 8.79527 2 12 2 12C2 12 5.63636 19 12 19C14.0503 19 15.8174 18.2734 17.2711 17.2884M11 5.05822C11.3254 5.02013 11.6588 5 12 5C18.3636 5 22 12 22 12C22 12 21.3082 13.3317 20 14.8335" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                     </path> <path d="M14 14.2362C13.4692 14.7112 12.7684 15.0001 12 15.0001C10.3431 15.0001 9 13.657 9 12.0001C9 11.1764 9.33193 10.4303 9.86932 9.88818" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                     </path> </g>
+             </svg>
+             :<svg onClick={() => setShowPassword(true)} viewBox="0 0 576 512" height="1em" xmlns="http://www.w3.org/2000/svg">
+             <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z">
+             </path>
+             </svg>}
+         </div>
+         
+         
+         {/* <div className="flex-row">
+             <div>
+             <input type="checkbox"/>
+             <label>Remember me </label>
+             </div>
+ 
+             <span className="span">Forgot password?</span>
+         </div> */}
+         <button className="button-submit" type="submit">Create Account</button>
+         <p className="p">Already have an account? <span className="span" onClick={() => setCurrentView('SIGN-IN')}>Sign In</span></p>
+         
+         {/* <p className="p line">Or With</p>
+ 
+         <div className="flex-row">
+         <button className="btn google">
+             <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                 <g clip-path="url(#clip0_0_347)">
+                 <path d="M13.9926 7.13096C13.9926 6.55738 13.9449 6.13883 13.8419 5.70477H7.1394V8.2936H11.0736C10.9943 8.93695 10.566 9.90584 9.61414 10.5569L9.6008 10.6436L11.72 12.2478L11.8668 12.2622C13.2152 11.0452 13.9926 9.25473 13.9926 7.13096Z" fill="#4285F4"/>
+                 <path d="M7.13948 13.9519C9.06691 13.9519 10.685 13.3318 11.8669 12.2622L9.61422 10.5569C9.0114 10.9677 8.20233 11.2545 7.13948 11.2545C5.2517 11.2545 3.64946 10.0376 3.07831 8.35565L2.99459 8.3626L0.791024 10.0291L0.762207 10.1074C1.93612 12.3862 4.34742 13.9519 7.13948 13.9519Z" fill="#34A853"/>
+                 <path d="M3.07816 8.35566C2.92746 7.92161 2.84024 7.45651 2.84024 6.97597C2.84024 6.49537 2.92746 6.03033 3.07023 5.59627L3.06624 5.50383L0.835056 3.81058L0.762055 3.84451C0.27823 4.79015 0.000610352 5.85207 0.000610352 6.97597C0.000610352 8.09987 0.27823 9.16173 0.762055 10.1074L3.07816 8.35566Z" fill="#FBBC05"/>
+                 <path d="M7.13948 2.69735C8.47995 2.69735 9.38417 3.26317 9.89976 3.73602L11.9145 1.81375C10.6771 0.689847 9.0669 0 7.13948 0C4.34742 0 1.93612 1.5657 0.762207 3.8445L3.07038 5.59626C3.64946 3.91428 5.2517 2.69735 7.13948 2.69735Z" fill="#EB4335"/>
+                 </g>
+                 <defs>
+                 <clipPath id="clip0_0_347">
+                 <rect width="14" height="14" fill="white"/>
+                 </clipPath>
+                 </defs>
+             </svg>
+             Google 
+         </button>
+         
+         <button className="btn apple">
+             <svg width="12" height="14" viewBox="0 0 12 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                 <g clip-path="url(#clip0_0_355)">
+                 <path d="M6.34516 1.21952C7.27201 0.00716167 8.56055 0.00125122 8.56055 0.00125122C8.56055 0.00125122 8.75222 1.14107 7.83144 2.23908C6.84828 3.41151 5.73078 3.21966 5.73078 3.21966C5.73078 3.21966 5.52094 2.29759 6.34516 1.21952ZM5.84866 4.0181C6.32548 4.0181 7.21043 3.36763 8.36232 3.36763C10.3451 3.36763 11.1251 4.76781 11.1251 4.76781C11.1251 4.76781 9.59954 5.54189 9.59954 7.42017C9.59954 9.53904 11.5 10.2693 11.5 10.2693C11.5 10.2693 10.1715 13.9801 8.37708 13.9801C7.55292 13.9801 6.91217 13.4289 6.04378 13.4289C5.15883 13.4289 4.28065 14.0007 3.70868 14.0007C2.0701 14.0007 0 10.4806 0 7.65098C0 4.86704 1.7522 3.40664 3.39569 3.40664C4.46411 3.40664 5.29321 4.0181 5.84866 4.0181Z" fill="#999999"/>
+                 </g>
+                 <defs>
+                 <clipPath id="clip0_0_355">
+                 <rect width="11.5" height="14" fill="white"/>
+                 </clipPath>
+                 </defs>
+             </svg>
+             Apple 
+             
+     </button>
+     </div> */}
+     </form>
+     </div>
+         </div>
+        )}
+       
 
         </div>
         </div>
